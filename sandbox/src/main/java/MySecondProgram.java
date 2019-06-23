f6d0a3fa0967ca15104641353a9f92a38f4b2810
Nina <nina.borynets@gmail.com>
2019-06-23 19:36:44 +0300

    Добавлено форматирование файла java

--- sandbox/src/main/java/MySecondProgram.java
+++ sandbox/src/main/java/MySecondProgram.java
@@ -1,6 +1,6 @@
-public class MySecondProgram{
+public class MySecondProgram {
 
- public static void main (String [] args){
+ public static void main (String [] args) {
 	System.out.println ("It's my homework!");
  }
  
