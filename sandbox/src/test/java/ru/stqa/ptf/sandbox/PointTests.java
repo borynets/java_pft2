c222141b856141493e8b159fffd7740856f67d9b
Nina <nina.borynets@gmail.com>
2019-06-28 16:31:47 +0300

    Добавлены тесты

--- sandbox/src/test/java/ru/stqa/ptf/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/ptf/sandbox/PointTests.java
@@ -7,9 +7,16 @@ import ru.stqa.pft.sandbox.Point;
 public class PointTests {
 
   @Test
-  public void distanceTest (){
+  public void distanceTest2 (){
     Point p = new Point(10, 8);
     Point p2 = new Point (6,4);
+    Assert.assertEquals(Point.distance(p,p2), 16);
+  }
+
+  @Test
+  public void distanceTest2 (){
+    Point p = new Point(22, 13);
+    Point p2 = new Point (10,7);
     Assert.assertEquals(Point.distance(p,p2), 16);
   }
 }
